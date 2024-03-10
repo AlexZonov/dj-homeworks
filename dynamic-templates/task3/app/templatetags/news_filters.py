6db5954c0ad9466a0594321c099228cba15422de
Flexford <flexford@mail.ru>
2024-03-10 18:07:30 +0300

    Completed dynamic-templates\task3 homework;

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -1,22 +1,63 @@
+import datetime
+import re
+
 from django import template
+from datetime import datetime as dt
 
 
 register = template.Library()
 
 
-@register.filter
+@register.filter(name="format_date")
 def format_date(value):
-    # Ваш код
+    if type(value) is not float:
+        return value
+
+    date = datetime.datetime.fromtimestamp(value, datetime.UTC)
+    now = dt.now(datetime.UTC)
+    diff = now - date
+    if diff.seconds / 60 < 10:
+        return "только что"
+    elif diff.days < 1:
+        return f'{int(diff.seconds / 60 / 60)} часов назад'
+    else:
+        return date.strftime("%Y-%m-%d")
+
+@register.filter(name="score")
+def score(value):
+    if type(value) is str:
+        return value
+
+    if type(value) is int:
+        if value < -5:
+            return "плохо"
+        elif value > 5:
+            return "хорошо"
+        elif value >= -5 and value <= 5:
+            return "нейтрально"
+
     return value
-
-
-# необходимо добавить фильтр для поля `score`
 
 
 @register.filter
 def format_num_comments(value):
-    # Ваш код
-    return value
+    if type(value) is int:
+        if value == 0:
+            return "Оставьте комментарий"
+        elif value < 50:
+            return value
+        elif value > 50:
+            return "50+"
 
+@register.filter
+def format_selftext(value, count):
+    stub_body = " ... "
+    pattern = r"([\w!@#$%^&*()']+)"
+    matches = list(re.finditer(pattern, value, re.M))
 
-
+    if len(matches) < (count * 2) + len(stub_body):
+        return value
+    else:
+        start_part = value[0:matches[count].regs[0][1]]
+        end_part = value[matches[-count].regs[0][0]:]
+        return f'{start_part}{stub_body}{end_part}'
