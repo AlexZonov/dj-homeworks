08efba55b8e4792bc4ab51d250ecae80ce53e9c0
Flexford <flexford@mail.ru>
2024-04-07 11:25:09 +0300

    implemented catalog logic

--- databases/work_with_database/phones/management/commands/import_phones.py
+++ databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,6 +2,8 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from datetime import datetime
+from django.utils.text import slugify
 
 
 class Command(BaseCommand):
@@ -15,6 +17,15 @@ class Command(BaseCommand):
             # пропускаем заголовок
             next(phone_reader)
 
+            Phone.objects.all().delete()
+
             for line in phone_reader:
                 # TODO: Добавьте сохранение модели
-                pass
+                record = Phone(id=int(line[0]),
+                               name=line[1],
+                               image=line[2],
+                               price=int(line[3]),
+                               release_date=datetime.strptime(line[4], "%Y-%m-%d"),
+                               lte_exists=bool(line[5]),
+                               slug=slugify(line[1]))
+                record.save()
