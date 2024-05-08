35a73241341261c304f9894d3cd4b2bab22d1f15
Flexford <flexford@mail.ru>
2024-05-08 13:59:42 +0300

    Completed orm_migrations homework;

--- databases_2/orm_migrations/school/admin.py
+++ databases_2/orm_migrations/school/admin.py
@@ -3,11 +3,19 @@ from django.contrib import admin
 from .models import Student, Teacher
 
 
+class StudentTeachers(admin.TabularInline):
+    model = Student.teacher.through
+
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
+    inlines = [StudentTeachers]
+    exclude = ('teacher', )
     pass
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
+    inlines = [StudentTeachers]
     pass
+
