73d5ec0c2555395dc2db647c1b510d71a00b471e
Flexford <flexford@mail.ru>
2024-05-10 17:25:35 +0300

    Completed car_admin homework

--- site-form-works/car_admin/app/admin.py
+++ site-form-works/car_admin/app/admin.py
@@ -3,14 +3,28 @@ from django.contrib import admin
 from .models import Car, Review
 from .forms import ReviewAdminForm
 
-
+@admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'brand', 'model', 'review_count']
+    list_display_links = ['id', 'brand', 'model', 'review_count']
+    search_fields = ('brand', 'model',)
+    list_filter = ('brand', 'model',)
+    verbose_name = "Машины"
+    verbose_name_plural = "Машины"
+
+    def get_ordering(self, request):
+        return ('-id',)
 
 
+@admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'car', 'title']
+    list_display_links = ['id', 'car', 'title']
+    search_fields = ('car', 'title',)
+    list_filter = ('car', 'title',)
     form = ReviewAdminForm
+    verbose_name = "Обзоры"
+    verbose_name_plural = "Обзоры"
 
-
-admin.site.register(Car, CarAdmin)
-admin.site.register(Review, ReviewAdmin)
+    def get_ordering(self, request):
+        return ('-id',)
--- site-form-works/car_admin/app/models.py
+++ site-form-works/car_admin/app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Car(models.Model):
-    brand = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50, verbose_name='Марка')
+    model = models.CharField(max_length=50, verbose_name='Модель')
 
     def __str__(self):
         return f'{self.brand} {self.model}'
@@ -11,12 +11,22 @@ class Car(models.Model):
     def review_count(self):
         return Review.objects.filter(car=self).count()
 
+    review_count.short_description = 'Кол-во обзоров'
+
+    class Meta:
+        verbose_name = "Машина"
+        verbose_name_plural = "Машины"
+
 
 class Review(models.Model):
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    text = models.TextField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name='Машина')
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
 
     def __str__(self):
         return str(self.car) + ' ' + self.title
+
+    class Meta:
+        verbose_name = "Обзор"
+        verbose_name_plural = "Обзоры"
 
