5aa904cb47b5b43142ed8f3a2fe97a690f8b1eef
Flexford <flexford@mail.ru>
2024-05-12 15:09:01 +0300

    Improved review homework(use class based view)

--- site-form-works/review/app/views.py
+++ site-form-works/review/app/views.py
@@ -2,6 +2,7 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponseBadRequest, HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
+from django.views import View
 
 from .models import Product, Review
 from .forms import ReviewForm
@@ -17,6 +18,50 @@ def product_list_view(request):
 
     return render(request, template, context)
 
+class product_view_new(View):
+    template = 'app/product_detail.html'
+
+    def get(self, request, pk):
+        product = get_object_or_404(Product, id=pk)
+        form = ReviewForm()
+        reviewed = self.is_reviewed(request, pk)
+        return self.render(request, form, product, reviewed)
+
+    def post(self, request, pk):
+        product = get_object_or_404(Product, id=pk)
+        form = ReviewForm(request.POST)
+        reviewed = self.is_reviewed(request, pk)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = product
+            user_id = request.session.session_key if request.session.session_key is not None else 'guest'
+            review.text = f'{user_id}: {review.text}'
+
+            if not reviewed:
+                session_reviews = request.session.get('reviews', list())
+                session_reviews.append(pk)
+                request.session['reviews'] = session_reviews
+                review.save()
+                return redirect('product_detail', pk=pk)
+            else:
+                form.errors['text'] = ['Вы уже оставляли отзыв!']
+
+        return self.render(request, form, product, reviewed)
+
+
+    def is_reviewed(self, request, pk):
+        return 'reviews' in request.session and pk in request.session['reviews']
+
+
+    def render(self, request, form, product, reviewed):
+        context = {
+            'form': form,
+            'product': product,
+            'reviews': product.reviews.all(),
+            'reviewed': reviewed
+        }
+
+        return render(request, self.template, context)
 
 def product_view(request : WSGIRequest, pk):
     template = 'app/product_detail.html'
