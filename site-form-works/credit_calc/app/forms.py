c1757b9513e6c32f4ff175646b5a4015e5bc6a3b
Flexford <flexford@mail.ru>
2024-05-12 17:17:33 +0300

    Complete credit_calc homework

--- site-form-works/credit_calc/app/forms.py
+++ site-form-works/credit_calc/app/forms.py
@@ -2,16 +2,16 @@ from django import forms
 
 
 class CalcForm(forms.Form):
-    initial_fee = forms.IntegerField(label="Стоимость товара")
-    rate = forms.CharField(label="Процентная ставка")
-    months_count = forms.IntegerField(label="Срок кредита в месяцах")
-
-    def clean_initial_fee(self):
-        # валидация одного поля, функция начинающаяся на `clean_` + имя поля
-        initial_fee = self.cleaned_data.get('initial_fee')
-        if not initial_fee or initial_fee < 0:
-            raise forms.ValidationError("Стоимость товара не может быть отрицательной")
-        return initial_fee
+    initial_fee = forms.IntegerField(label="Стоимость товара", min_value=1)
+    rate = forms.FloatField(label="Процентная ставка", min_value=1, max_value=100)
+    months_count = forms.IntegerField(label="Срок кредита в месяцах", min_value=1, max_value=360)
+    #
+    # def clean_initial_fee(self):
+    #     # валидация одного поля, функция начинающаяся на `clean_` + имя поля
+    #     initial_fee = self.cleaned_data.get('initial_fee')
+    #     if not initial_fee or initial_fee < 0:
+    #         raise forms.ValidationError("Стоимость товара не может быть отрицательной")
+    #     return initial_fee
 
     def clean(self):
         # общая функция валидации
--- site-form-works/credit_calc/app/views.py
+++ site-form-works/credit_calc/app/views.py
@@ -5,10 +5,25 @@ from .forms import CalcForm
 
 def calc_view(request):
     template = "app/calc.html"
+    common_result = 0
+    result = 0
+    has_result = 0
 
-    form = CalcForm
+
+    form = CalcForm(request.GET)
+    if form.is_valid():
+        fee = form.cleaned_data['initial_fee']
+        percents = form.cleaned_data['rate'] / 100
+        months = form.cleaned_data['months_count']
+        common_result = fee + (fee * percents)
+        result = common_result / months
+        has_result = True
+
     context = {
-        'form': form
+        'form': form,
+        'common_result': common_result,
+        'result': result,
+        'has_result': has_result
     }
 
     return render(request, template, context)
