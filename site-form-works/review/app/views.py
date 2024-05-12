45c1aa75f35f3b489101ab85f261fc441437295f
Flexford <flexford@mail.ru>
2024-05-12 14:48:51 +0300

    Completed review homework

--- site-form-works/review/app/views.py
+++ site-form-works/review/app/views.py
@@ -1,3 +1,5 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponseBadRequest, HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 
@@ -16,18 +18,38 @@ def product_list_view(request):
     return render(request, template, context)
 
 
-def product_view(request, pk):
+def product_view(request : WSGIRequest, pk):
     template = 'app/product_detail.html'
     product = get_object_or_404(Product, id=pk)
+    reviews = product.reviews.all()
+    form = None
 
-    form = ReviewForm
+    session_reviews = request.session.get('reviews', list())
+    reviewed = pk in session_reviews
+
     if request.method == 'POST':
-        # логика для добавления отзыва
-        pass
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = product
+            user_id = request.session.session_key if request.session.session_key is not None else 'guest'
+            review.text = f'{user_id}: {review.text}'
+
+            if not reviewed:
+                session_reviews.append(pk)
+                request.session['reviews'] = session_reviews
+                review.save()
+                return redirect('product_detail', pk=pk)
+            else:
+                form.errors['text'] = ['Вы уже оставляли отзыв!']
+    else:
+        form = ReviewForm
 
     context = {
         'form': form,
-        'product': product
+        'product': product,
+        'reviews': reviews,
+        'reviewed': reviewed
     }
 
     return render(request, template, context)
